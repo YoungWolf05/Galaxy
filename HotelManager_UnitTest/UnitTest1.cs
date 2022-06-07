e4b7448a47b0220fb50f643ea92f9134fef9765a
Choon-Feng Sng <choonfeng.sng05@hotmail.com>
2022-06-07 23:53:05 +0800

    Add Nunit of HotelManager

---
+++ HotelManager_UnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+using static HotelManager.ManagementTool;
+using NUnit.Framework;
+
+namespace HotelManager_UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
